3eabd375ee40ed60691bb59c5f8b0c6106defb7d
beatahoss <beata@Beatas-MacBook-Air.local>
2020-06-16 16:20:37 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const myButton = document.getElementById("btn");
+const mySecret = document.getElementById("secret");
+
+myButton.addEventListener("click", () => {
+  mySecret.style.display = "block";
+});
