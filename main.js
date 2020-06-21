6317ef766ff05e874eddb62da7af961d6fb09988
Beata Hosseini <beahoss@MacBook-Air.local>
2020-06-21 22:49:03 +0100

    added the second button

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+// get the first button
 const myButton = document.getElementById("btn");
+// get the secret paragraph
 const mySecret = document.getElementById("secret");
+// get the don't click me button
+const myDontClickMeButton = document.getElementById("dontclickme");
 
 myButton.addEventListener("click", () => {
   mySecret.style.display = "block";
+});
+
+myDontClickMeButton.addEventListener("click", () => {
+  alert("Told ya not to do so, bad boy!!!");
 });
